ef102e1009be24a79cdc08e9cebf74f614080384
Phil Hawksworth <phil@netify.com>
2019-01-06 12:49:20 +0000

    avoid memory issues by doing this in series. Slower but more reliable

--- capture.js
+++ capture.js
@@ -1,6 +1,6 @@
 const axios     = require('axios');
 const puppeteer = require('puppeteer');
-
+var browser, page;
 
 /*
 * Global config variables
@@ -17,12 +17,9 @@ const conf = {
 async function snap(url, file) {
   try {
     console.log('snapping :', url);
-    let browser = await puppeteer.launch({ headless: true });
-    let page = await browser.newPage();
     await page.setViewport({ width: 1024, height: 512 });
     await page.goto(url);
     await page.screenshot({ path: file, type: 'png' });
-    await browser.close();
     console.log('snapped :', file);
   }
   catch (err) {
@@ -31,17 +28,27 @@ async function snap(url, file) {
 }
 
 
+async function setupAndProcess(array) {
+  browser = await puppeteer.launch({ headless: true });
+  page = await browser.newPage();
+  for (const url of array) {
+    let re = /\//gi;
+    let filename = url.split("://")[1].replace(re,".");
+    let path = `${conf.path}/${filename}.png`
+    await snap(url, path);
+  }
+  browser.close();
+  console.log('Done!');
+}
+
+
 /*
 * Discover the list of urls for which we need snapshots
 */
+
 axios.get(conf.index)
   .then((response) => {
-    response.data.cards.forEach( (url) => {
-      let re = /\//gi;
-      let filename = url.split("://")[1].replace(re,".");
-      let path = `${conf.path}/${filename}.png`
-      snap(url, path);
-    });
+    setupAndProcess(response.data.cards);
   })
   .catch((error) => {
     console.log('error :', error)
